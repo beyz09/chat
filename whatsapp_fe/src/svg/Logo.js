ac034e4d7dca2ff476c55c730811dd5b58e2eef1
beyzanur baği <114877090+beyz09@users.noreply.github.com>
2025-01-06 14:23:37 +0300

    Update Logo.js

--- whatsapp_fe/src/svg/Logo.js
+++ whatsapp_fe/src/svg/Logo.js
@@ -1,4 +1,4 @@
-import logo from 'c:/Users/loveo/Desktop/chatApp/whatsapp_fe/src/assets/logo192.png';
+import logo from 'c:/Users/loveo/Desktop/chatApp/whatsapp_fe/src/assets/logo192.png'; //assets klasöründeki logo192.png nin yolunu ekle
 
 export default function Logo() {
   return (
